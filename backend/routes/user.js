9e8cd08591d09e42b79453ab3bc3be65d5ec05ae
Lasai-Freyr <ferry.thomas@hotmail.fr>
2020-10-12 16:18:49 +0200

    ptites modif backend route user

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -6,9 +6,9 @@ const rateLimiter = require("../middleware/rateLimiter");
 const userCtrl = require('../controllers/user.js');
 const auth = require('../middleware/auth');
 
-//const verifyPassword = require('../middleware/verifyPassword');
+const verifyPassword = require('../middleware/verifyPassword');
 
-router.post('/signup',rateLimiter, userCtrl.signup);
+router.post('/signup', rateLimiter, verifyPassword, userCtrl.signup);
 router.post('/login',rateLimiter, userCtrl.login);
 router.delete('/:id', auth, userCtrl.delteteAccount);
 router.get('/:id', auth,  userCtrl.findUser)
